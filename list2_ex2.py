90dc2bcdc94a28abec649bcab6e21fb1a14e0531
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-08 15:05:18 -0300

    2 questão respondida

---
+++ list2_ex2.py
@@ -0,0 +1,10 @@
+"""
+ Faça um programa em linguagem Python que 
+ leia dois números inteiros e informe se estes são iguais ou diferentes.
+"""
+num1 = float(input('Informe o primeiro número: '))
+num2 = float(input('Informe o segundo número: '))
+if num1 == num2:
+    print(f'Os números informados são iguais')
+else:
+    print(f'Os números informados são diferentes')
