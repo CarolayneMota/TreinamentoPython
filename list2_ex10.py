553d83743cf4c2637012eb6543fc67919c4df6ca
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-08 16:59:12 -0300

    10 questão respondida

---
+++ list2_ex10.py
@@ -0,0 +1,16 @@
+"""
+Escreva um programa Python para verificar se uma letra do Alfabeto(Abecedário) é uma vogal ou consoante.
+"""
+letra = input('Informe uma letra e saiba de é vogal ou consoante: ')
+if letra == 'a' or letra == 'A':
+    print(f'A letra {letra} é vogal')
+elif letra == 'e' or letra == 'E':
+    print(f'A letra {letra} é vogal')
+elif letra == 'i' or letra == 'I':
+    print(f'A letra {letra} é vogal')
+elif letra == 'o' or letra == 'O':
+    print(f'A letra {letra} é vogal')
+elif letra == 'u' or letra == 'U':
+    print(f'A letra {letra} é vogal')
+else:
+    print(f'A letra {letra} é consoante')
