3748c96210ec3f6b4eb7403d47617fa07e7b12a6
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-08 15:17:16 -0300

    2 questão respondida

--- list2_ex3.py
+++ list2_ex3.py
@@ -4,3 +4,10 @@ exibido na tela “Gastos dentro do orçamento” caso o valor gasto não
 ultrapasse o valor do salário e “Orçamento estourado” se o valor gasto
 ultrapassar o valor do salário.
 """
+salario = float(input('Informe o sálario recebido: '))
+total = float(input('Informe o total gasto com despesas: '))
+orcamento = salario - total
+if orcamento > 0:
+    print('Gastos dentro do orçamento')
+else:
+    print('Orçamento estourado')
