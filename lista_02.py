b409785df8c71e8e6181f11d428d87dda1067ac6
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-08 11:01:03 -0300

    Ex. 12 foi feita

--- lista_02.py
+++ lista_02.py
@@ -143,9 +143,16 @@ else:
     print('Valor Inválido!')
 
 12-
+num = int(input('Informe um número: '))
+if num % 2 == 0:
+    print(f'O número {num} é par')
+else:
+    print(f'O número {num} é impar')
 
 13-
 
 14-
+
+15-
 
 """
