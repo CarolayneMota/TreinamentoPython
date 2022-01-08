d800720f39dc21e9aaa23e32224629477652ce34
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-07 23:18:12 -0300

    terminei a primeira questão

---
+++ lista_02.py
@@ -0,0 +1,36 @@
+"""
+1 -
+num1 = float(input('Informe o primeiro número: '))
+num2 = float(input('Informe o segundo número: '))
+if num1 > num2:
+    print(f'{num1} é maior que {num2}')
+else:
+    print(f'{num2} é maior que {num1}')
+
+2-
+
+3-
+
+4-
+
+5-
+
+6-
+
+7-
+
+8-
+
+9-
+
+10-
+
+11-
+
+12-
+
+13-
+
+14-
+
+"""
