440de8856d807b1ff98ae622e6f222d2a60f55ba
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-08 16:46:25 -0300

    8 questão respondida

---
+++ list2_ex8.py
@@ -0,0 +1,11 @@
+"""
+Escreva um programa em Python para encontrar números entre 100 e 400 (ambos inclusos), onde 
+cada dígito de um número é um número par. Os números obtidos devem ser impressos em sequência 
+separada por vírgulas.
+"""
+l = []
+for i in range(100,401):
+    s = str(i)
+    if (int(s[0]) % 2 == 0) and (int(s[1]) % 2 == 0) and (int(s[2]) % 2 == 0):
+        l.append(s)
+print(','.join(l))
