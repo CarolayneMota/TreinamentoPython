cf2dd5e1691b2f5295c0c6bc7a9188a68c73c598
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-10 16:12:58 -0300

    Duas questões respondidas

---
+++ aula_04.py
@@ -0,0 +1,21 @@
+"""
+1 - Crie um programa que tenha uma lista de 5 frutas, e depois adicione uma nova fruta no final da lista. 
+Depois remova o primeiro elemento da lista e por fim, altere o valor do item 2 pra "banana"
+lista = ['maçã', 'banana','uva', 'mamão', 'pera']
+print(lista)
+lista.append('melão')
+print(lista)
+lista.pop(0)
+lista[1] = 'banana'
+print(lista)
+2 - Crie uma lista com os dados de 4 pessoas (nome, idade, sexo, ano_nascimento).
+Dica:  Usar lista de lista
+dados = []
+print(dados)
+dados.append(['Carolayne', 20, 'Feminino', 2001])
+dados.append(['Maria Victoria', 20, 'Feminino', 2001])
+dados.append(['Thammara', 20, 'Feminino', 2001])
+dados.append(['Jennife', 20, 'Feminino', 2001])
+print(dados)
+"""
+
