3f5c715af99a8fd821bc441dc6ea5b786479389e
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-11 16:13:13 -0300

    5 questão respondia

--- lista_03.py
+++ lista_03.py
@@ -61,6 +61,11 @@ for i in range(qtd_pessoas):
 
 
 5-
+Faça um programa que imprima na tela os números de 1 a 20, um abaixo do
+outro.(usar for com range)
+
+for i in range(1, 21):
+    print(i)
 
 6-
 
