3e938e7b928a08b8e8aafa9696e4b872f36db311
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-11 15:32:11 -0300

    3 questão respondida

--- lista_03.py
+++ lista_03.py
@@ -12,6 +12,7 @@ print(f'Lista: {lista}')
 2-
 Faça um Programa que leia 10 números reais, insira em uma lista e
 mostre-os na ordem crescente (use a função sort())
+
 lista = []
 for i in range(10):
     num = float(input('Informe um número: '))
@@ -20,6 +21,20 @@ lista.sort()
 print(f'Lista: {lista}')
 
 3-
+Faça um Programa que leia N (peça pro usuário informar o valor de N) notas
+e insira em uma lista, depois percorra a lista e calcule a soma das notas, por
+fim calcule a média (soma dividido por N) e mostre na tela.
+
+lista = []
+soma = 0
+n = int(input('Informe o valor N: '))
+for i in range(n):
+    num = float(input('Informe um número: '))
+    lista.append(num)
+for i in range(n):
+    soma = soma + lista[i]
+media = soma/n
+print(f'A media é {media}')
 
 4-
 
