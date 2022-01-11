66d69d29f4ada818cab6bda3ae53fd80eecd28fe
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-11 14:54:34 -0300

    1 questão respondida

---
+++ lista_03.py
@@ -0,0 +1,36 @@
+"""
+1 - 
+Faça um Programa que leia 5 números inteiros, insira em uma lista e
+mostre-os.
+
+lista = []
+for i in range(5):
+    num = int(input('Informe um número: '))
+    lista.append(num)
+print(f'Lista: {lista}')
+
+2-
+
+3-
+
+4-
+
+5-
+
+6-
+
+7-
+
+8-
+
+9-
+
+10 -
+
+----------Desafios----------
+1-
+
+2-
+
+"""
+
