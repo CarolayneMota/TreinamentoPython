ad5f1be304d2f608f4c80a681077218246d4cf74
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-13 14:14:42 -0300

    Added return na função

--- geometria/main.py
+++ geometria/main.py
@@ -1,10 +1,12 @@
 import geometria as geo
-opcao = int(input('Informe o número da figura que você que saber a área.\n1. Circulo\n2. Quadrado'))
+opcao = int(input('Informe o número da figura que você que saber a área.\n1. Circulo\n2. Quadrado\nR: '))
 if opcao == 1:
     raio = float(input('Informe o raio em cm: '))
-    geo.area_circulo(raio)
+    area = geo.area_circulo(raio)
+    print(f'A área do círculo de raio {raio} é {area}cm')
 elif opcao == 2:
     tam = float(input('Informe o tamanho do lado em cm: '))
-    geo.area_quadrado(tam)
+    area = geo.area_quadrado(tam)
+    print(f'A área do quadrado de lado {tam}cm é {area}cm ')
 else:
     print('Opção invalida')
