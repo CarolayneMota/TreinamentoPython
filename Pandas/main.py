8c648e52e79ecc2d19d35cbfbd16f08e387309d0
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-18 21:51:10 -0300

    pandas

---
+++ Pandas/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+"""
+lista = [1985, 2000, 3000]
+
+serie = pd.Series(lista)
+print(serie)
+
+
+for dado in serie:
+    print(dado)
+
+print(serie.describe())
+
+Funções: iloc, where 
+"""
