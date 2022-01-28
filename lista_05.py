f6e48d06707647ef2b24f61cf2138b0e9ece4e5f
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-28 11:48:06 -0300

    desafio 2 feito

--- lista_05.py
+++ lista_05.py
@@ -154,6 +154,24 @@ Desafios
 1 -
 
 2 -
+class Triangulo:
+    def __init__(self, lado_a, lado_b,lado_c):
+        self.a = lado_a
+        self.b = lado_b
+        self.c = lado_c
+
+    def perimetro(self):
+        p = self.a + self.b + self.c
+        return p
+
+    def tipo_lado(self):
+        if self.a == self.b == self.c:
+            return 'equilátero'
+        elif self.a != self.b and self.b != self.c:
+            return 'escaleno'
+        else:
+            return 'isóceles'
+
 
 """
 
